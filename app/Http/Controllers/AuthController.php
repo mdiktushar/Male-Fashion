d57846fb7a8934de29270b94e9c993e891411e99
mdiktushar <md.tushar1804@gmail.com>
2023-10-23 17:29:27 +0600

    authController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,4 +7,23 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     //
+    public function register (Request $request) {
+
+    }
+
+    public function signup (Request $request) {
+        
+    }
+
+    public function logout (Request $request) {
+        
+    }
+
+    public function changePassword (Request $request) {
+        
+    }
+
+    public function resetPassword (Request $request) {
+        
+    }
 }

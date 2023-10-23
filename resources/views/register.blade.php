ac89464a8aca0c26477dacb838055eb49ca2327d
mdiktushar <md.tushar1804@gmail.com>
2023-10-23 18:21:02 +0600

    register blade

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -18,31 +18,31 @@
                             @csrf
                             <div class="row">
                                 <div class="col-lg-12">
-                                    <input name="fullname" type="text" placeholder="Full Name">
+                                    <input name="fullname" type="text" placeholder="Full Name" class="@error('fullname') border border-danger @enderror">
                                     @error('fullname')
                                         <p class="text-danger">{{$message}}</p>
                                     @enderror
                                 </div>
                                 <div class="col-lg-12">
-                                    <input name="email" type="text" placeholder="Email">
+                                    <input name="email" type="text" placeholder="Email" class="@error('email') border border-danger @enderror">
                                     @error('email')
                                         <p class="text-danger">{{$message}}</p>
                                     @enderror
                                 </div>
                                 <div class="col-lg-12">
-                                    <input name="photo" type="file" class="form-control" placeholder="Photo URL">
+                                    <input name="photo" type="file" class="form-control @error('photo') border border-danger @enderror" placeholder="Photo URL" >
                                     @error('photo')
                                         <p class="text-danger">{{$message}}</p>
                                     @enderror
                                 </div>
                                 <div class="col-lg-6">
-                                    <input name="password" type="password" placeholder="Password">
+                                    <input name="password" type="password" placeholder="Password" class="@error('password') border border-danger @enderror">
                                     @error('password')
                                         <p class="text-danger">{{$message}}</p>
                                     @enderror
                                 </div>
                                 <div class="col-lg-6">
-                                    <input name="password_confirmation" type="password" placeholder="Conferm Password">
+                                    <input name="password_confirmation" type="password" placeholder="Conferm Password" class="@error('password') border border-danger @enderror">
                                 </div>
                                 <div class="col-lg-12">
                                     <button type="submit" class="site-btn">Register</button>

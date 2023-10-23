6ee1a0387ef3ec818188ef9d55d5792c69da0e3b
mdiktushar <md.tushar1804@gmail.com>
2023-10-23 14:33:28 +0600

    role migrations ;

--- database/migrations/2023_10_23_080839_create_roles_table.php
+++ database/migrations/2023_10_23_080839_create_roles_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('fullname');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('picture');
-            $table->boolean('is_active')->default(false);
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
     }
 };
--- database/migrations/2023_10_23_082839_create_role_user_table.php
+++ database/migrations/2023_10_23_082839_create_role_user_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('fullname');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('picture');
-            $table->boolean('is_active')->default(false);
+        Schema::create('role_user', function (Blueprint $table) {
+            $table->primary(['user_id', 'role_id']);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('role_user');
     }
 };

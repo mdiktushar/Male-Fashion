2c228f4cd0a9daef3d56e27073608580af6c1cbc
mdiktushar <md.tushar1804@gmail.com>
2023-10-24 20:26:59 +0600

    user migrate update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -11,10 +12,20 @@ class AuthController extends Controller
     //
     public function register (RegisterRequest $request) {
         // dd($request);
+        $user = User::create([
+            'fullname' => $request->fullname,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        $this->userLogin($request->email, $request->password);
     }
 
     public function login (LoginRequest $request) {
         // dd($request);
+    }
+
+    private function userLogin ($email, $password) {
+        dd($email, $password);
     }
 
     public function logout (Request $request) {
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('password');
             $table->string('picture');
             $table->boolean('is_active')->default(false);
+            $table->integer('otp')->nullable();
             $table->timestamps();
         });
     }

14127dbe26b92cea42654ddfde44c78dac112045
mdiktushar <md.tushar1804@gmail.com>
2023-10-27 16:03:01 +0600

    email verification route is created

--- resources/views/emails/emailVerification.blade.php
+++ resources/views/emails/emailVerification.blade.php
@@ -1,7 +1,7 @@
 <x-mail::message>
 # Introduction
 Hello {{$name}} <br>
-Please Verify your email address for the Auth system application.
+Please Verify your email address for your account.
 
 <x-mail::button :url="'http://127.0.0.1:8000/auth/verify-email/'.$url">
 Button Text
--- routes/Web/auth.php
+++ routes/Web/auth.php
@@ -20,3 +20,5 @@ Route::get('/login', [MainController::class, 'login'])->name('loginPage');
 Route::post('register', [AuthController::class, 'register'])->name('signup');
 Route::post('login', [AuthController::class, 'login'])->name('login');
 Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('auth/verify-email/{code}', [AuthController::class, 'emailValidation'])->name('emailValidation');

65b9ccf3db1ae198f33cd65a5f9d77bdc23b55d8
mdiktushar <md.tushar1804@gmail.com>
2023-10-29 17:09:28 +0600

    category migration created

--- database/migrations/2023_10_28_085539_create_products_table.php
+++ database/migrations/2023_10_28_085539_create_products_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
 
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            
             $table->string('title');
             $table->string('picture');
             $table->string('description');

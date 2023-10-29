acc6b38fa3add90755f74a38226265062553f1fb
mdiktushar <md.tushar1804@gmail.com>
2023-10-29 17:51:25 +0600

    admin route resource;

--- routes/Web/admin.php
+++ routes/Web/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,3 +13,5 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::resource('admin/', AdminController::class);

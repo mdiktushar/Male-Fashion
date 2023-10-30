e140107cdbd96b412a4c3c22d89821662aa4e3c9
mdiktushar <md.tushar1804@gmail.com>
2023-10-30 12:43:33 +0600

    add product page

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -131,9 +131,9 @@
                 </div>
                 <div class="col-lg-3 col-md-3">
                     <div class="header__nav__option">
-                        <a href="#" class="search-switch"><img src="img/icon/search.png" alt=""></a>
-                        <a href="#"><img src="img/icon/heart.png" alt=""></a>
-                        <a href="#"><img src="img/icon/cart.png" alt=""> <span>0</span></a>
+                        <a href="#" class="search-switch"><img src={{asset("img/icon/search.png")}} alt=""></a>
+                        <a href="#"><img src={{asset("img/icon/heart.png")}} alt=""></a>
+                        <a href="#"><img src={{asset("img/icon/cart.png")}} alt=""> <span>0</span></a>
                         <div class="price">$0.00</div>
                     </div>
                 </div>
--- resources/views/pages/admin/addProduct.blade.php
+++ resources/views/pages/admin/addProduct.blade.php
@@ -19,42 +19,60 @@
                         <form action={{ route('signup') }} method="POST" enctype="multipart/form-data">
                             @csrf
                             <div class="row">
+
                                 <div class="col-lg-12">
-                                    <input name="fullname" type="text" placeholder="Full Name"
-                                        class="@error('fullname') border border-danger @enderror">
-                                    @error('fullname')
+                                    <input name="title" type="text" placeholder="Product Name"
+                                        class="@error('title') border border-danger @enderror">
+                                    @error('title')
                                         <p class="text-danger">{{ $message }}</p>
                                     @enderror
                                 </div>
+
                                 <div class="col-lg-12">
-                                    <input name="email" type="text" placeholder="Email"
-                                        class="@error('email') border border-danger @enderror">
-                                    @error('email')
+                                    <input name="picture" type="file"
+                                        class="form-control @error('picture') border border-danger @enderror"
+                                        placeholder="picture URL">
+                                    @error('picture')
                                         <p class="text-danger">{{ $message }}</p>
                                     @enderror
                                 </div>
+
                                 <div class="col-lg-12">
-                                    <input name="photo" type="file"
-                                        class="form-control @error('photo') border border-danger @enderror"
-                                        placeholder="Photo URL">
-                                    @error('photo')
+                                    <textarea name="description" class="form-control @error('description') border border-danger @enderror"
+                                        placeholder="Description"></textarea>
+                                    @error('description')
                                         <p class="text-danger">{{ $message }}</p>
                                     @enderror
                                 </div>
+
                                 <div class="col-lg-6">
-                                    <input name="password" type="password" placeholder="Password"
-                                        class="@error('password') border border-danger @enderror">
-                                    @error('password')
+                                    <input name="price" type="number" placeholder="Price"
+                                        class="@error('price') border border-danger @enderror">
+                                    @error('price')
                                         <p class="text-danger">{{ $message }}</p>
                                     @enderror
                                 </div>
+
                                 <div class="col-lg-6">
-                                    <input name="password_confirmation" type="password" placeholder="Conferm Password"
-                                        class="@error('password') border border-danger @enderror">
+                                    <input name="quantity" type="number" placeholder="Quantity"
+                                        class="@error('quantity') border border-danger @enderror">
+                                    @error('quantity')
+                                        <p class="text-danger">{{ $message }}</p>
+                                    @enderror
                                 </div>
+
+                                <select name="category"
+                                    class="col-lg-11 form-control mx-auto @error('category') border border-danger @enderror">
+                                    <option value="null" selected>Category</option>
+                                    <option value="Footwear">Footwear</option>
+                                    <option value="T-Shirt">T-Shirt</option>
+                                </select>
+
                                 <div class="col-lg-12">
+                                    <br>
                                     <button type="submit" class="site-btn">Register</button>
                                 </div>
+
                             </div>
                         </form>
                     </div>

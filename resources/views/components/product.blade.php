cc42173fc1a0f4acc310988b409cea217dc1b11a
mdiktushar <md.tushar1804@gmail.com>
2023-11-02 18:45:18 +0600

    product component is added to shop page

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -1,3 +1,36 @@
-<div>
-    <!-- Order your soul. Reduce your wants. - Augustine -->
+<div class="col-lg-4 col-md-6 col-sm-6">
+    <div class="product__item">
+        <div class="product__item__pic set-bg" data-setbg="img/product/product-4.jpg">
+            <ul class="product__hover">
+                <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
+                <li><a href="#"><img src="img/icon/compare.png" alt="">
+                        <span>Compare</span></a>
+                </li>
+                <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
+            </ul>
+        </div>
+        <div class="product__item__text">
+            <h6>Diagonal Textured Cap</h6>
+            <a href="#" class="add-cart">+ Add To Cart</a>
+            <div class="rating">
+                <i class="fa fa-star-o"></i>
+                <i class="fa fa-star-o"></i>
+                <i class="fa fa-star-o"></i>
+                <i class="fa fa-star-o"></i>
+                <i class="fa fa-star-o"></i>
+            </div>
+            <h5>$60.9</h5>
+            <div class="product__color__select">
+                <label for="pc-10">
+                    <input type="radio" id="pc-10">
+                </label>
+                <label class="active black" for="pc-11">
+                    <input type="radio" id="pc-11">
+                </label>
+                <label class="grey" for="pc-12">
+                    <input type="radio" id="pc-12">
+                </label>
+            </div>
+        </div>
+    </div>
 </div>
--- resources/views/pages/public/shop.blade.php
+++ resources/views/pages/public/shop.blade.php
@@ -207,44 +207,12 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-lg-4 col-md-6 col-sm-6">
-                            {{-- <div class="product__item sale">
-                                    <div class="product__item__pic set-bg" data-setbg="img/product/product-3.jpg">
-                                        <span class="label">Sale</span>
-                                        <ul class="product__hover">
-                                            <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
-                                            <li><a href="#"><img src="img/icon/compare.png" alt="">
-                                                    <span>Compare</span></a>
-                                            </li>
-                                            <li><a href="#"><img src="img/icon/search.png" alt=""></a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="product__item__text">
-                                        <h6>Multi-pocket Chest Bag</h6>
-                                        <a href="#" class="add-cart">+ Add To Cart</a>
-                                        <div class="rating">
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star"></i>
-                                            <i class="fa fa-star-o"></i>
-                                        </div>
-                                        <h5>$43.48</h5>
-                                        <div class="product__color__select">
-                                            <label for="pc-7">
-                                                <input type="radio" id="pc-7">
-                                            </label>
-                                            <label class="active black" for="pc-8">
-                                                <input type="radio" id="pc-8">
-                                            </label>
-                                            <label class="grey" for="pc-9">
-                                                <input type="radio" id="pc-9">
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div> --}}
-                        </div>
 
+                        {{-- Product Component Start --}}
+                        @foreach ($products as $item)
+                            <x-product :product="$item" />
+                        @endforeach
+                        {{-- Product Component End --}}
                     </div>
                     <div class="row">
                         <div class="col-lg-12">

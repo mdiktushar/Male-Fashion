019f408c2afa58bd41b82108dff7a96ccc306ea4
mdiktushar <md.tushar1804@gmail.com>
2023-11-02 18:38:06 +0600

    product component created

---
+++ app/View/Components/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Product extends Component
+{
+    public $product;
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($product)
+    {
+        //
+        $this->product = $product;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.product');
+    }
+}

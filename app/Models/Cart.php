f34c1d2de04d532d735321b4d3eb048cac1d2383
mdiktushar <md.tushar1804@gmail.com>
2023-11-03 22:20:47 +0600

    cart modle belongsTo methord

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -9,5 +9,26 @@ class Cart extends Model
 {
     use HasFactory;
     protected $guarded = [];
-    
+
+
+    /**
+     * Get the user that owns the Cart
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    /**
+     * Get the user that owns the Cart
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function Product()
+    {
+        return $this->belongsTo(Product::class, 'foreign_key', 'other_key');
+    }
 }

e6db929aae2d7f4cf3115594d8dd6481adc53a6d
mdiktushar <md.tushar1804@gmail.com>
2023-11-03 21:26:17 +0600

    product component add to cart button

--- resources/views/components/products/product/product.blade.php
+++ resources/views/components/products/product/product.blade.php
@@ -12,7 +12,7 @@
     </div>
     <div class="product__item__text">
         <h6>{{ $product->title }}</h6>
-        <a href="#" class="add-cart">+ Add To Cart</a>
+        <a href={{ route('singleProductPage', $product) }} class="add-cart">+ Add To Cart</a>
         <div class="rating">
             <i class="fa fa-star-o"></i>
             <i class="fa fa-star-o"></i>

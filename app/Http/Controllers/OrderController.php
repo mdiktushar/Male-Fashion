c09cf49f5b18a066908819b25666f8aa435be362
mdiktushar <md.tushar1804@gmail.com>
2023-11-05 18:28:46 +0600

    place order route

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
     //
+    public function placeOrder (Request $request) {
+        
+    }
 }
--- routes/Web/order.php
+++ routes/Web/order.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,3 +13,5 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::post('/place_Order', [OrderController::class, 'placeOrder'])->name('placeOrder');

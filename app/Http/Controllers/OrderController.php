aca86535f98f6e71444cb5ef1f8a7bd264b76acf
mdiktushar <md.tushar1804@gmail.com>
2023-11-05 20:15:39 +0600

    place order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -8,16 +8,30 @@ use Illuminate\Http\Request;
 class OrderController extends Controller
 {
     //
-    public function placeOrder (Request $request) {
-        // $input = $request->validate([
-        //     'fullname' => 'required',
-        //     'address' => 'required',
-        //     'phone' => 'required',
-        // ]);
+    public function placeOrder(Request $request)
+    {
+        $input = $request->validate([
+            'fullname' => 'required',
+            'address' => 'required',
+            'phone' => 'required',
+        ]);
+        $user = auth()->user();
 
-        $carts = auth()->user()->carts()->get();
-        
+        $order = $user->orders()->create();
 
+        $carts = $user->carts()->get();
+
+        foreach ($carts as $cart) {
+            $input['product_id'] = $cart->product_id;
+            $input['quantity'] = $cart->quantity;
+            $input['price'] = $cart->product()->first()->price;
+
+            $order->orderItems()->create($input);
+
+            $cart->delete();
+        }
+
+        session()->flash('success', 'Order Plased');
         return redirect()->back();
     }
 }

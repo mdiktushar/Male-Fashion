545e6edb85dd07d8d21b3e646bb1929fbdcb0280
mdiktushar <md.tushar1804@gmail.com>
2023-11-05 18:44:54 +0600

    #2 migration file for orderItem

--- database/migrations/2023_11_05_120140_create_order_items_table.php
+++ database/migrations/2023_11_05_120140_create_order_items_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->unsignedBigInteger('product_id');
             $table->foreign('product_id')->references('id')->on('products');
 
+            $table->integer('quantity');
             $table->float('price');
 
             $table->string('fullname');

69c7371c998990fe5b0808cd6fad1af6dd929baf
mdiktushar <md.tushar1804@gmail.com>
2023-11-16 13:39:19 +0600

    blade setup for profile

--- resources/views/pages/user/profile.blade.php
+++ resources/views/pages/user/profile.blade.php
@@ -1,51 +1,46 @@
 @extends('layout.mainLayout')
 
 @section('content')
-<!-- Contact Section Begin -->
-<section class="contact spad">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 col-md-6">
-                <div class="contact__text">
-                    <div class="section-title">
-                        <span>Information</span>
-                        <h2>Contact Us</h2>
-                        <p>As you might expect of a company that began as a high-end interiors contractor, we pay
-                            strict attention.</p>
+    <!-- Contact Section Begin -->
+    <section class="contact spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 mx-auto">
+                    <div class="d-flex justify-content-center text-center mb-4">
+                        <img class="rounded-circle border border-dark" width="150px" height="150px" src={{ auth()->user()->picture }}
+                            alt="">
                     </div>
-                    <ul>
-                        <li>
-                            <h4>America</h4>
-                            <p>195 E Parker Square Dr, Parker, CO 801 <br />+43 982-314-0958</p>
-                        </li>
-                        <li>
-                            <h4>France</h4>
-                            <p>109 Avenue Léon, 63 Clermont-Ferrand <br />+12 345-423-9893</p>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-6 col-md-6">
-                <div class="contact__form">
-                    <form action="#">
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <input type="text" placeholder="Name">
+                    <div class="contact__form">
+                        <form action="#">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <label class="text-muted">Full Name</label>
+                                    <input type="text" placeholder="{{ auth()->user()->fullname }}">
+                                </div>
+                                <div class="col-lg-6">
+                                    <label class="text-muted">Email</label>
+                                    <input type="text" placeholder="{{ auth()->user()->email }}">
+                                </div>
+
+                                <div class="col-lg-12">
+                                    <label class="text-muted">Photo</label>
+                                    <input name="photo" type="file"
+                                        class="form-control @error('photo') border border-danger @enderror"
+                                        placeholder="Photo URL">
+                                    @error('photo')
+                                        <p class="text-danger">{{ $message }}</p>
+                                    @enderror
+                                </div>
+
+                                <div class="col-lg-12">
+                                    <button class="btn-block site-btn" type="submit" class="site-btn">Update</button>
+                                </div>
                             </div>
-                            <div class="col-lg-6">
-                                <input type="text" placeholder="Email">
-                            </div>
-                            <div class="col-lg-12">
-                                <textarea placeholder="Message"></textarea>
-                                <button type="submit" class="site-btn">Send Message</button>
-                            </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
-<!-- Contact Section End -->
-    
+    </section>
+    <!-- Contact Section End -->
 @endsection

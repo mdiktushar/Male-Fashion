46f1c0b05edf8487c78c55452c5b77ab128d6695
mdiktushar <md.tushar1804@gmail.com>
2023-12-15 22:04:41 +0600

    admin layout, header and footer

---
+++ resources/views/components/admin/admin-footer.blade.php
@@ -0,0 +1,46 @@
+        <!-- partial:partials/_footer.html -->
+        <footer class="footer">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2021. Premium <a
+                        href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from
+                    BootstrapDash. All rights reserved.</span>
+                <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i
+                        class="ti-heart text-danger ml-1"></i></span>
+            </div>
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Distributed by <a
+                        href="https://www.themewagon.com/" target="_blank">Themewagon</a></span>
+            </div>
+        </footer>
+        <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+        </div>
+        <!-- container-scroller -->
+
+        <!-- plugins:js -->
+        <script src="vendors/js/vendor.bundle.base.js"></script>
+        <!-- endinject -->
+        <!-- Plugin js for this page -->
+        <script src="vendors/chart.js/Chart.min.js"></script>
+        <script src="vendors/datatables.net/jquery.dataTables.js"></script>
+        <script src="vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
+        <script src="js/dataTables.select.min.js"></script>
+
+        <!-- End plugin js for this page -->
+        <!-- inject:js -->
+        <script src="js/off-canvas.js"></script>
+        <script src="js/hoverable-collapse.js"></script>
+        <script src="js/template.js"></script>
+        <script src="js/settings.js"></script>
+        <script src="js/todolist.js"></script>
+        <!-- endinject -->
+        <!-- Custom js for this page-->
+        <script src="js/dashboard.js"></script>
+        <script src="js/Chart.roundedBarCharts.js"></script>
+        <!-- End custom js for this page-->
+        </body>
+
+        </html>
---
+++ resources/views/layout/adminLayout.blade.php
@@ -0,0 +1,10 @@
+    <!-- Header Component Begin -->
+    <x-admin.admin-header />
+    <!-- Header Component End -->
+
+    @yield('admin-content')
+
+
+    <!-- Footer Component Begin -->
+    <x-admin.admin-footer />
+    <!-- Footer Component End -->

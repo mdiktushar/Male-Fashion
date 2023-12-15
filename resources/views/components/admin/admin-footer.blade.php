75329ce41aa780cae4fe6446d9bd36844eff7af0
mdiktushar <md.tushar1804@gmail.com>
2023-12-15 22:15:42 +0600

    dynamic styling and js

--- resources/views/components/admin/admin-footer.blade.php
+++ resources/views/components/admin/admin-footer.blade.php
@@ -21,25 +21,25 @@
         <!-- container-scroller -->
 
         <!-- plugins:js -->
-        <script src="vendors/js/vendor.bundle.base.js"></script>
+        <script src={{ asset('admin/vendors/js/vendor.bundle.base.js') }}></script>
         <!-- endinject -->
         <!-- Plugin js for this page -->
-        <script src="vendors/chart.js/Chart.min.js"></script>
-        <script src="vendors/datatables.net/jquery.dataTables.js"></script>
-        <script src="vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
-        <script src="js/dataTables.select.min.js"></script>
+        <script src={{ asset('admin/vendors/chart.js/Chart.min.js') }}></script>
+        <script src={{ asset('admin/vendors/datatables.net/jquery.dataTables.js') }}></script>
+        <script src={{ asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}></script>
+        <script src={{ asset('admin/js/dataTables.select.min.js') }}></script>
 
         <!-- End plugin js for this page -->
         <!-- inject:js -->
-        <script src="js/off-canvas.js"></script>
-        <script src="js/hoverable-collapse.js"></script>
-        <script src="js/template.js"></script>
-        <script src="js/settings.js"></script>
-        <script src="js/todolist.js"></script>
+        <script src={{ asset('admin/js/off-canvas.js') }}></script>
+        <script src={{ asset('admin/js/hoverable-collapse.js') }}></script>
+        <script src={{ asset('admin/js/template.js') }}></script>
+        <script src={{ asset('admin/js/settings.js') }}></script>
+        <script src={{ asset('admin/js/todolist.js') }}></script>
         <!-- endinject -->
         <!-- Custom js for this page-->
-        <script src="js/dashboard.js"></script>
-        <script src="js/Chart.roundedBarCharts.js"></script>
+        <script src={{ asset('admin/js/dashboard.js') }}></script>
+        <script src={{ asset('admin/js/Chart.roundedBarCharts.js') }}></script>
         <!-- End custom js for this page-->
         </body>
 
--- resources/views/components/admin/admin-header.blade.php
+++ resources/views/components/admin/admin-header.blade.php
@@ -7,19 +7,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Skydash Admin</title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="vendors/feather/feather.css">
-    <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css">
-    <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+    <link rel="stylesheet" href={{ asset('admin/vendors/feather/feather.css') }}>
+    <link rel="stylesheet" href={{ asset('admin/vendors/ti-icons/css/themify-icons.css') }}>
+    <link rel="stylesheet" href={{ asset('admin/vendors/css/vendor.bundle.base.css') }}>
     <!-- endinject -->
     <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="vendors/datatables.net-bs4/dataTables.bootstrap4.css">
-    <link rel="stylesheet" href="vendors/ti-icons/css/themify-icons.css">
-    <link rel="stylesheet" type="text/css" href="js/select.dataTables.min.css">
+    <link rel="stylesheet" href={{ asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}>
+    <link rel="stylesheet" href={{ asset('admin/vendors/ti-icons/css/themify-icons.css') }}>
+    <link rel="stylesheet" type={{ asset('admin/text/css" href="js/select.dataTables.min.css') }}>
     <!-- End plugin css for this page -->
     <!-- inject:css -->
-    <link rel="stylesheet" href="css/vertical-layout-light/style.css">
+    <link rel="stylesheet" href={{ asset('admin/css/vertical-layout-light/style.css') }}>
     <!-- endinject -->
-    <link rel="shortcut icon" href="images/favicon.png" />
+    <link rel="shortcut icon" href={{ asset('admin/images/favicon.png') }} />
 </head>
 
 <body>

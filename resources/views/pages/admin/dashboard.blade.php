814dc7a9fe5c6de02fee399646a045acfc24fb37
mdiktushar <md.tushar1804@gmail.com>
2024-02-09 15:49:27 +0600

    dahsboard editing

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -5,25 +5,9 @@
         <div class="col-md-12 grid-margin">
             <div class="row">
                 <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                    <h3 class="font-weight-bold">Welcome Aamir</h3>
+                    <h3 class="font-weight-bold">Welcome {{auth()->user()->fullname}}</h3>
                     <h6 class="font-weight-normal mb-0">All systems are running smoothly! You have <span
-                            class="text-primary">3 unread alerts!</span></h6>
-                </div>
-                <div class="col-12 col-xl-4">
-                    <div class="justify-content-end d-flex">
-                        <div class="dropdown flex-md-grow-1 flex-xl-grow-0">
-                            <button class="btn btn-sm btn-light bg-white dropdown-toggle" type="button"
-                                id="dropdownMenuDate2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                <i class="mdi mdi-calendar"></i> Today (10 Jan 2021)
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuDate2">
-                                <a class="dropdown-item" href="#">January - March</a>
-                                <a class="dropdown-item" href="#">March - June</a>
-                                <a class="dropdown-item" href="#">June - August</a>
-                                <a class="dropdown-item" href="#">August - November</a>
-                            </div>
-                        </div>
-                    </div>
+                            class="text-primary">No alerts!</span></h6>
                 </div>
             </div>
         </div>
@@ -32,15 +16,15 @@
         <div class="col-md-6 grid-margin stretch-card">
             <div class="card tale-bg">
                 <div class="card-people mt-auto">
-                    <img src="images/dashboard/people.svg" alt="people">
+                    <img src={{asset("admin/images/dashboard/people.svg")}} alt="people">
                     <div class="weather-info">
                         <div class="d-flex">
                             <div>
                                 <h2 class="mb-0 font-weight-normal"><i class="icon-sun mr-2"></i>31<sup>C</sup></h2>
                             </div>
                             <div class="ml-2">
-                                <h4 class="location font-weight-normal">Bangalore</h4>
-                                <h6 class="font-weight-normal">India</h6>
+                                <h4 class="location font-weight-normal">Dhaka</h4>
+                                <h6 class="font-weight-normal">Bangladesh</h6>
                             </div>
                         </div>
                     </div>
@@ -90,7 +74,7 @@
             </div>
         </div>
     </div>
-    <div class="row">
+    {{-- <div class="row">
         <div class="col-md-6 grid-margin stretch-card">
             <div class="card">
                 <div class="card-body">
@@ -720,7 +704,7 @@
                     <div class="row">
                         <div class="col-12">
                             <div class="table-responsive">
-                                {{-- <table id="example" class="display expandable-table" style="width:100%">
+                                <table id="example" class="display expandable-table" style="width:100%">
                                             <thead>
                                                 <tr>
                                                     <th>Quote#</th>
@@ -733,7 +717,7 @@
                                                     <th></th>
                                                 </tr>
                                             </thead>
-                                        </table> --}}
+                                        </table>
                             </div>
                         </div>
                     </div>
@@ -742,5 +726,5 @@
 
 
         </div>
-    </div>
+    </div> --}}
 @endsection

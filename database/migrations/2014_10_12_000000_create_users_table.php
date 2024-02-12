4c1901844b9e79037a06fb25fe990ac80b380ed3
mdiktushar <md.tushar1804@gmail.com>
2024-02-12 19:25:05 +0600

    migration update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('fullname');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->length(191);;
             $table->string('password');
             $table->string('picture');
             $table->string('picture_delete_url');
--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
+            $table->string('uuid')->unique()->length(191);;
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
